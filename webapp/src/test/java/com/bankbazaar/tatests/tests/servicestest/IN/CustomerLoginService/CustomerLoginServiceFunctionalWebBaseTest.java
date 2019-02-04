03347969cc4c628fcbf25f83d8f8b53a54cd4f35
SivarajRadhakrishnan <sivaraj.radhakrishnan@bankbazaar.com>
2019-02-04 20:41:01 +0530

    Functional Web Test changes

--- webapp/src/test/java/com/bankbazaar/tatests/tests/servicestest/IN/CustomerLoginService/CustomerLoginServiceFunctionalWebBaseTest.java
+++ webapp/src/test/java/com/bankbazaar/tatests/tests/servicestest/IN/CustomerLoginService/CustomerLoginServiceFunctionalWebBaseTest.java
@@ -1,7 +1,30 @@
 package com.bankbazaar.tatests.tests.servicestest.IN.CustomerLoginService;
 
+import com.bankbazaar.tatests.services.Services;
+import com.bankbazaar.tatests.tests.servicestest.ServicesPipelineDataDrivenBaseTest;
+
 /**
  * Created by sivaraj on 6/12/18.
  */
-public class CustomerLoginServiceFunctionalWebBaseTest {
+public class CustomerLoginServiceFunctionalWebBaseTest extends ServicesPipelineDataDrivenBaseTest {
+    @Override
+    public void beforeSuite() {
+
+    }
+
+    @Override
+    public void beforeClass() {
+        super.init(Services.CustomerLoginService());
+        dataKeyName = "Data_Key";
+    }
+
+    @Override
+    public void afterSuite() {
+
+    }
+
+    @Override
+    public void afterClass() {
+
+    }
 }
--- webapp/src/test/java/com/bankbazaar/tatests/tests/servicestest/IN/CustomerLoginService/CustomerLoginServiceFunctionalWebTest.java
+++ webapp/src/test/java/com/bankbazaar/tatests/tests/servicestest/IN/CustomerLoginService/CustomerLoginServiceFunctionalWebTest.java
@@ -1,45 +1,47 @@
 package com.bankbazaar.tatests.tests.servicestest.IN.CustomerLoginService;
 
+import com.bankbazaar.core.BaseContext;
 import com.bankbazaar.model.TestInfo;
+import com.bankbazaar.tatests.constants.web.MPConstants;
+import com.bankbazaar.tatests.pages.Pages;
+import com.bankbazaar.tatests.services.Services;
+import com.bankbazaar.tatests.workflows.WorkFlows;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Map;
 
 /**
  * Created by sivaraj on 6/12/18.
  */
 public class CustomerLoginServiceFunctionalWebTest extends CustomerLoginServiceFunctionalWebBaseTest {
-    @Test(groups = {"Regression","GetCustomerById"},
-            description = "Call getCustomerById API with existing cusomer Id of BANKBAZAAR customer")
+    @Test(groups = {"Regression","SignIn"},
+            description = "Login into MP using facebook user credentials and verify login is success")
     @TestInfo(testLinkTestCaseIds = {"NA"},
-            testCaseDescription = "Call getCustomerById API with existing cusomer Id of BANKBAZAAR customer",
-            useCasesDescription = "Call getCustomerById API with existing cusomer Id of BANKBAZAAR customer",
-            module = "GetCustomerById",
+            testCaseDescription = "Login into MP using facebook user credentials and verify login is success",
+            useCasesDescription = "Login into MP using facebook user credentials and verify login is success",
+            module = "SignIn",
             category = "CUSTOMERLOGINSERVICE",
             testType = "Services",
             currentStage = "InRegression",
             priority = "P1",
             createdBy = "sivaraj.radhakrishnan@bankbazaar.com",
-            createdOn = "10/09/2018",
+            createdOn = "03/01/2019",
             timeToAutomateInHrs = "")
-    public void IN_CustomerLoginService_FunctionalWeb_TestCase1()
+    public void IN_CustomerLoginService_Validate_FB_Login()
     {
-        Assert.assertEquals(true, true);
-    }
-    @Test(groups = {"Regression","GetCustomerById"},
-            description = "Call getCustomerById API with existing cusomer Id of BANKBAZAAR customer")
-    @TestInfo(testLinkTestCaseIds = {"NA"},
-            testCaseDescription = "Call getCustomerById API with existing cusomer Id of BANKBAZAAR customer",
-            useCasesDescription = "Call getCustomerById API with existing cusomer Id of BANKBAZAAR customer",
-            module = "GetCustomerById",
-            category = "CUSTOMERLOGINSERVICE",
-            testType = "Services",
-            currentStage = "InRegression",
-            priority = "P1",
-            createdBy = "sivaraj.radhakrishnan@bankbazaar.com",
-            createdOn = "10/09/2018",
-            timeToAutomateInHrs = "")
-    public void IN_CustomerLoginService_FunctionalWeb_TestCase2()
-    {
-        Assert.assertEquals(true, false);
+        Map<String, String> values = Services.CustomerLoginService().getAllWebValues();
+        BaseContext.testContext.get().partnerBankName = values.get(MPConstants.nonPartner);
+        Pages.HomePage().loadHomePage(values);
+        Pages.Dashboard().click_loginButton();
+        Pages.LoginPage().validate_isDisplayedloginToBankbazaar(values);
+        Pages.LoginPage().validate_isDisplayedSignInWithFacebook(values.get(MPConstants.signInWithFacebook));
+        Pages.LoginPage().validate_isSelectedExistingUser();
+        Pages.LoginPage().click_fbLoginButton();
+        Pages.LoginPage().validate_isDisplayedFbRedirectionText(values.get(MPConstants.fbRedirection));
+        Pages.FbLogin().login_facebook(values.get(MPConstants.fbLoginUserName), values.get(MPConstants.fbLoginPassword), false);
+        Pages.Dashboard().validate_isDisplayedSuccessLogin();
+        WorkFlows.DashBoardWF().validate_SuccessLogin();
+        EndTest();
     }
 }
